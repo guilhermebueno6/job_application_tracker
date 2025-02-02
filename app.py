5cfc9923c1332aac4fa4649aa1e4b8f993438b04
Guilherme Bueno <guilhermebueno6@gmail.com>
2025-02-02 00:24:02 -0500

    adding scraping to linkedin

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/add-application')
+def addApplication():
+    return render_template("add-application.html")
+
+@app.route('/add-new-application', methods=['POST'])
+def addNewApplication():
+    link = request.form['link']
+    print(link)
+    return link
